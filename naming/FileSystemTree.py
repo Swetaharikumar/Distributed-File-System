4e12b44a2ed4262a562c5de5946ca36b3b15065c
tejasri1 <tthota@andrew.cmu.edu>
2020-03-28 13:16:48 -0700

    working checkpoint naming tests..

--- naming/FileSystemTree.py
+++ naming/FileSystemTree.py
@@ -2,11 +2,19 @@ from collections import defaultdict
 
 
 class File(object):
-    def __init__(self, name):
+    def __init__(self, name, ip=None, port=None):
         self.map = defaultdict(File)
         self.name = name
         self.isFile = True
         self.value = -1
+        self.ip = ip
+        self.port = port
+
+    def to_string(self):
+        out = ""
+        for k, v in self.map.items():
+            out = out + v.name + "  "
+        return "name:  " + self.name + " | isFile: " + str(self.isFile) + " | children: " + out
 
 class FileSystem(object):
 
@@ -14,31 +22,53 @@ class FileSystem(object):
         self.root = File("/")
         self.root.isFile = False
 
-    def insert(self, path):
+    def search(selfself, path):
         """
         :type path: str
         :rtype: bool
         """
         array = path.split("/")
         cur = self.root
-        for i in range(1, len(array)):
+        for i in range(0, len(array)):
             name = array[i]
+            if name == '':
+                continue
             if name not in cur.map:
-                cur.map[name] = File(name)
-                cur.isFile = False
+                return False
 
             cur = cur.map[name]
         return True
 
+
+    def insert(self, path, ip=None, port=None):
+        """
+        :type path: str
+        :rtype: void
+        """
+        array = path.split("/")
+        cur = self.root
+        for i in range(0, len(array)):
+            name = array[i]
+            if name == '':
+                continue
+            if name not in cur.map:
+                cur.map[name] = File(name, ip, port)
+                cur.isFile = False
+
+            cur = cur.map[name]
+        return
+
     # Returns the File object
     def get(self, path):
         cur = self.root
-        if (path == "/" or path == "//"):
+        if (path == "/" or path == "//" or path == ''):
             return cur
 
         array = path.split("/")
-        for i in range(1, len(array)):
+        for i in range(0, len(array)):
             name = array[i]
+            if name == '':
+                continue
             if name not in cur.map:
                 return None
             cur = cur.map[name]
--- naming/namingServer.py
+++ naming/namingServer.py
@@ -10,16 +10,24 @@ namingRegister = Flask('namingRegister')  # Creating the Registration web server
 fs = FileSystem()
 
 def startNamingService():
-    print("Starting naming service")
+    # print("Starting naming service")
     namingService.run(host='localhost', port=sys.argv[1])
 
 def startNamingRegister():
-    print("Starting naming Registration")
+    # print("Starting naming Registration")
     namingRegister.run(host='localhost', port=sys.argv[2])
 
 
 def make_response(content, status_code):
     response = namingService.response_class(
+        response=content,
+        status=status_code,
+        mimetype='application/json'
+    )
+    return response
+
+def make_register_response(content, status_code):
+    response = namingRegister.response_class(
         response=content,
         status=status_code,
         mimetype='application/json'
@@ -42,13 +50,38 @@ def isValidPath():
     return response
 
 
+@namingService.route('/getstorage', methods=['POST'])
+def getStorage():
+    pathJson = request.get_json()
+    path = pathJson["path"]
+
+    # Not a valid path
+    if not isValidPathHelper (path):
+        constant.exceptionReturn["exception_type"] = "IllegalArgumentException"
+        constant.exceptionReturn["exception_info"] = "[getstorage] given path is invalid"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_response(content, 404)
+        return response
+
+    # Non-existent file and reject directory
+    node  = fs.get(path)
+    if not node or not node.isFile:
+        constant.exceptionReturn["exception_type"] = "FileNotFoundException"
+        constant.exceptionReturn["exception_info"] = "[getstorage] File/path cannot be found."
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_response(content, 404)
+        return response
+
+    getStorageReturn = {"server_ip": node.ip, "server_port": node.port}
+    content = json.dumps(getStorageReturn)
+    response = make_response(content, 200)
+    return response
+
 
 @namingService.route('/list', methods=['POST'])
 def list():
     pathJson = request.get_json()
     path = pathJson["path"]
-    if path == '//':
-        path = '/'
 
     if not isValidPathHelper (path):
         constant.exceptionReturn["exception_type"] = "IllegalArgumentException"
@@ -65,13 +98,14 @@ def list():
         response = make_response(content, 404)
         return response
 
+    filesReturn = {"files" : []}
 
     for k,v in node.map.items():
-        constant.filesReturn['files'].append(v.name)
+        if k not in filesReturn['files']:
+            filesReturn['files'].append(k)
 
-    print (constant.filesReturn['files'])
-    # constant.filesReturn['files'] = list(set(constant.filesReturn['files']))
-    content = json.dumps(constant.filesReturn)
+    # constant.filesReturn['files'] = list(set(filesReturn['files']))
+    content = json.dumps(filesReturn)
     response = make_response(content, 200)
     return response
 
@@ -90,7 +124,6 @@ def isDirectory():
 
     node = fs.get(path)
     if not node:
-        constant.boolReturn["success"] = False
         constant.exceptionReturn["exception_type"] = "FileNotFoundException"
         constant.exceptionReturn["exception_info"] = "[is_directory] File/path cannot be found."
         content =  json.dumps(constant.exceptionReturn)
@@ -103,20 +136,75 @@ def isDirectory():
     return response
 
 
+@namingService.route('/create_file', methods=['POST'])
+def createFile():
+    pathJson = request.get_json()
+    path = pathJson["path"]
+    if not isValidPathHelper(path):
+        constant.exceptionReturn["exception_type"] = "IllegalArgumentException"
+        constant.exceptionReturn["exception_info"] = "[create_file / create_directory] given path is invalid"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_response(content, 404)
+        return response
+
+    return createHelper(path)
+
+
+@namingService.route('/create_directory', methods=['POST'])
+def createDir():
+    pathJson = request.get_json()
+    path = pathJson["path"]
+
+    if not isValidPathHelper(path):
+        constant.exceptionReturn["exception_type"] = "IllegalArgumentException"
+        constant.exceptionReturn["exception_info"] = "[create_file / create_directory] given path is invalid"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_response(content, 404)
+        return response
+    return createHelper(path, True)
+
+
+def createHelper(path, createDir = None):
+    if not isValidPathHelper(path):
+        constant.exceptionReturn["exception_type"] = "IllegalArgumentException"
+        constant.exceptionReturn["exception_info"] = "[create_file / create_directory] given path is invalid"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_response(content, 404)
+        return response
+
+    parent_path = path.rsplit('/', 1)[0]
+    parent_node = fs.get(parent_path)
+    # Reject when parent directory does not exist | parent dir is a file
+    if not parent_node or parent_node.isFile:
+        constant.exceptionReturn["exception_type"] = "FileNotFoundException"
+        constant.exceptionReturn["exception_info"] = "[create_file / create_directory] File/path cannot be found."
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_response(content, 404)
+        return response
+
+    cur_node = fs.get(path)
+    # Return false when file already exists
+    if cur_node:
+        constant.boolReturn["success"] = False
+        content = json.dumps(constant.boolReturn)
+        response = make_response(content, 200)
+        return response
+
+    fs.insert(path) ## ip, port?? which storage server to assign
+    constant.boolReturn["success"] = True
+    content = json.dumps(constant.boolReturn)
+    response = make_response(content, 200)
+    if createDir:
+        node = fs.get(path)
+        node.isFile = False
+    return response
+
+
 def isValidPathHelper (path):
     if not path or ':' in path or path[0] != '/':
         return False
     else:
         return True
-
-
-def isDirectoryHelper(path):
-    valid = False
-    for file in constant.filesDict.keys():
-        if file.startswith (path + '/') or path == '/':
-            valid = True
-            break
-    return valid
 
 
 @namingRegister.route('/register', methods = ['POST'])
@@ -136,7 +224,7 @@ def register():
             constant.exceptionReturn["exception_info"] = "This storage client already registered."
 
             content =  json.dumps(constant.exceptionReturn)
-            response = make_response(content, 409)
+            response = make_register_response(content, 409)
             return response
 
     for reqFile in ssReq['files']:
@@ -147,13 +235,14 @@ def register():
         if node:
             deleteFiles['files'].append(reqFile)
         else:
-            fs.insert(reqFile)
+            print("inserting ", reqFile)
+            fs.insert(reqFile, ssReq['storage_ip'], ssReq['client_port'])
             storageServerEntry['files'].append(reqFile)
 
     # For now simple append, later look at files and send delete command for duplicates
     constant.storageServers.append(storageServerEntry)
     content = json.dumps(deleteFiles)
-    response = make_response(content, 200);
+    response = make_register_response(content, 200);
     return response;
 
 

84774eba204680736cfe535d95511eda1bd28602
tejasri1 <tthota@andrew.cmu.edu>
2020-03-29 15:11:07 -0700

    working Access test

--- storage/consts.py
+++ storage/consts.py
@@ -3,3 +3,7 @@ namingServerPort = None
 namingServerRegister = "/register"
 
 myip = "127.0.0.1"
+
+
+boolReturn = {"success": None}
+exceptionReturn = {"exception_type": None, "exception_info": None}
--- storage/storage_server.py
+++ storage/storage_server.py
@@ -8,7 +8,7 @@ import glob
 import os
 import shutil
 import logging
-
+import base64
 
 clientService = Flask('clientServer')  # Creating the client servcie web server
 commandService = Flask('commandServer')  # Creating the command web server
@@ -40,11 +40,7 @@ def register ():
     headers = {'Content-type': 'application/json'}
     r = requests.post(url=url, data=json.dumps(data), headers=headers)
     res = r.json()
-    logging.error("original: ")
-    logging.error(data['files'])
 
-    logging.error ("To remove")
-    logging.error(res)
     remove_files (res['files'], storagePath)
 
 
@@ -63,10 +59,7 @@ def prune(path):
         for name in dirs:
             file_paths.append(os.path.join(root, name)[len(path):])
 
-    logging.info("Before prune")
-    logging.info(file_paths)
 
-    logging.info ("##############")
     for root, dirs, files in os.walk(path, topdown=False):
         for name in dirs:
             prune_path = os.path.join(root, name)
@@ -105,11 +98,146 @@ def make_command_response(content, status_code):
     )
     return response
 
+def isValidPathHelper (path):
+    if not path or ':' in path or path[0] != '/':
+        return False
+    else:
+        return True
+
 
 @commandService.route('/storage_delete', methods=['POST'])
 def storageDelete():
     pathJson = request.get_json()
     path = pathJson["path"]
+
+
+@clientService.route('/storage_size', methods=['POST'])
+def storageSize():
+    pathJson = request.get_json()
+    path = pathJson["path"]
+    storagePath = sys.argv[4]
+    if not isValidPathHelper(path):
+        constant.exceptionReturn["exception_type"] = "IllegalArgumentException"
+        constant.exceptionReturn["exception_info"] = "[storage_size] given path is invalid"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_client_response(content, 404)
+        return response
+
+    filepath = storagePath + path
+    if not os.path.isfile(filepath) or os.path.isdir(filepath):
+        constant.exceptionReturn["exception_type"] = "FileNotFoundException"
+        constant.exceptionReturn["exception_info"] = "[storage_size] given path is absent or a directory"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_client_response(content, 404)
+        return response
+
+    filepath = storagePath + path
+    content = {"size" : os.path.getsize(filepath)}
+    response = make_client_response(json.dumps(content), 200);
+    return response
+
+
+@clientService.route('/storage_read', methods=['POST'])
+def storageRead():
+    pathJson = request.get_json()
+    path = pathJson["path"]
+    offset = pathJson["offset"]
+    length = pathJson["length"]
+
+    storagePath = sys.argv[4]
+    if not isValidPathHelper(path):
+        constant.exceptionReturn["exception_type"] = "IllegalArgumentException"
+        constant.exceptionReturn["exception_info"] = "[storage_read] given path is invalid"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_client_response(content, 404)
+        return response
+
+    filepath = storagePath + path
+    if not os.path.isfile(filepath) or os.path.isdir(filepath):
+        constant.exceptionReturn["exception_type"] = "FileNotFoundException"
+        constant.exceptionReturn["exception_info"] = "[storage_read] given path is absent or a directory"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_client_response(content, 404)
+        return response
+
+    filesize = os.path.getsize(filepath)
+    if offset < 0 or length < 0 or  length > filesize or (filesize != 0 and offset >= filesize):
+        constant.exceptionReturn["exception_type"] = "IndexOutOfBoundsException"
+        constant.exceptionReturn["exception_info"] = "[storage_read] Negative offset/length | Length exceeded filesize"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_client_response(content, 404)
+        return response
+
+    try :
+        f = open(filepath,"rb")
+        f.seek(offset, 0)
+        # allow_length = min(length, os.path.getsize(filepath))
+        data = f.read(length)
+        f.close()
+
+        encoded_data = base64.b64encode(data).decode('ascii')
+        content = {"data" : encoded_data}
+        response = make_client_response(json.dumps(content), 200)
+    except:
+        constant.exceptionReturn["exception_type"] = "IOException"
+        constant.exceptionReturn["exception_info"] = "[storage_read] IOException"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_client_response(content, 404)
+
+    return response
+
+
+
+@clientService.route('/storage_write', methods=['POST'])
+def storageWrite():
+    pathJson = request.get_json()
+    path = pathJson["path"]
+    offset = pathJson["offset"]
+    encoded_data = pathJson["data"]
+
+    storagePath = sys.argv[4]
+    if not isValidPathHelper(path):
+        constant.exceptionReturn["exception_type"] = "IllegalArgumentException"
+        constant.exceptionReturn["exception_info"] = "[storage_write] given path is invalid"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_client_response(content, 404)
+        return response
+
+    filepath = storagePath + path
+    if not os.path.isfile(filepath) or os.path.isdir(filepath):
+        constant.exceptionReturn["exception_type"] = "FileNotFoundException"
+        constant.exceptionReturn["exception_info"] = "[storage_write] given path is absent or a directory"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_client_response(content, 404)
+        return response
+
+    if offset < 0 :
+        constant.exceptionReturn["exception_type"] = "IndexOutOfBoundsException"
+        constant.exceptionReturn["exception_info"] = "[storage_write] Negative offset"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_client_response(content, 404)
+        return response
+
+
+    try :
+        data = base64.b64decode(encoded_data)
+        f = open(filepath,"wb")
+        f.seek(offset, 0)
+        f.write(data)
+        f.close()
+
+        constant.boolReturn["success"] = True
+        response = make_client_response(json.dumps(constant.boolReturn), 200)
+    except:
+        constant.exceptionReturn["exception_type"] = "IOException"
+        constant.exceptionReturn["exception_info"] = "[storage_read] IOException"
+        content =  json.dumps(constant.exceptionReturn)
+        response = make_client_response(content, 404)
+
+    return response
+
+
+
 
 
 if __name__ == "__main__":

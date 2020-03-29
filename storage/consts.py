2cf3a987719991d76d7af292027ea70264ab34a2
tejasri1 <tthota@andrew.cmu.edu>
2020-03-29 10:27:47 -0700

    minimal working storage registration

---
+++ storage/consts.py
@@ -0,0 +1,5 @@
+namingServerUrl = "http://localhost:"
+namingServerPort = None
+namingServerRegister = "/register"
+
+myip = "127.0.0.1"
--- storage/storage_server.py
+++ storage/storage_server.py
@@ -3,11 +3,15 @@ from threading import Thread
 import json
 import sys
 import requests
+import consts as constant
+import glob
+import os
 
 
 clientService = Flask('clientServer')  # Creating the client servcie web server
 commandService = Flask('commandServer')  # Creating the command web server
-namingServerUrl = "localhost:"
+
+# storagePath =
 
 def startClientService():
     print("Starting client service")
@@ -15,12 +19,33 @@ def startClientService():
 
 def startCommandService():
     print("Starting command service")
-
-    # call naming server's register api
-    # r = requests.get(url = URL, params = PARAMS)
-    # data = r.json()
-
+    print(glob.glob(str(sys.argv[4])))
+    register()
     commandService.run(host='localhost', port=sys.argv[2])
+
+
+# call naming server's register api
+def register ():
+    constant.namingServerPort = sys.argv[3]
+
+
+    url = constant.namingServerUrl + constant.namingServerPort + constant.namingServerRegister
+    data = {"storage_ip": constant.myip,
+            "client_port": sys.argv[1],
+            "command_port" : sys.argv[2],
+            "files" : get_file_paths(sys.argv[4])}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    r = requests.post(url=url, data=json.dumps(data), headers=headers)
+    print("response from naming server: ", r.json())
+
+def get_file_paths(path):
+    file_paths = []
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            file_paths.append(os.path.join(root, name)[len(path):])
+        # for name in dirs:
+        #     file_paths.append(os.path.join(root, name)[len(path):])
+    return file_paths
 
 
 def make_client_response(content, status_code):
